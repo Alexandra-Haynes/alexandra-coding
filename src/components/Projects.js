21d9d2550e6174a017a32797bc39966ed782502b
Alexandra Haynes <sandamunteanua@yahoo.com>
2023-08-19 16:18:15 -1000

    refactored code for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,158 +1,71 @@
 import React from "react";
 import { BsCode, BsEye } from "react-icons/bs";
 
-const ProjectCard = ({
-  title,
-  description,
-  tools,
-  image,
-  color,
-  githubLink,
-  liveLink,
-}) => {
+const LinkButton = ({ link, color, icon, text }) => {
   return (
-    <>
-      <div
-        className={`border mx-auto shadow-lg h-1/4 
-        grid  grid-cols-projectsCard        
-        grid-rows-5 max-w-[1000px] bg-white`}
-      >
-        <div className={`h-full w-2 row-span-5 bg-${color}`}></div>
-        <div
-          className="row-span-5 flex flex-col justify-around pl-8 py-8
-        "
-        >
-          <h1 className="text-lg pb-2">{title}</h1>
-          <div className="h-[1px] w-1/2 bg-black/10 mb-6"></div>
-          <p className="max-w-[400px] ">{description}</p>
-
-          <div className="flex flex-row gap-4 pt-6">
-            <a
-              href={liveLink}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={` flex items-center justify-center 
-        gap-1 border shadow-lg border-slate-400 p-1 px-2
-        hover:scale-[102%] ease-in-out transition-all hover:shadow-xl
-         bg-${color}`}
-            >
-              <BsEye />
-              Live Project
-            </a>
-            <a
-              href={githubLink}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center justify-center 
-      gap-1 px-2 shadow-lg border border-slate-400 p-1 
-      hover:scale-[102%] ease-in-out transition-all hover:shadow-xl"
-            >
-              <BsCode />
-              Source Code
-            </a>
-          </div>
-
-          <ul className="flex flex-row gap-2 py-6">
-            {tools.map((tool, index) => (
-              <li key={index}>
-                <img
-                  src={`/${tool}.png`}
-                  alt={tool}
-                  width="40px"
-                  height="40px"
-                  title={tool}
-                />
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div
-          className="h-full row-span-5 overflow-hidden object-center
-        flex items-center justify-center "
-        >
-          <img src={image} className=" max-h-[340px]" />
-        </div>
-      </div>
-    </>
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`flex items-center justify-center gap-1 border shadow-lg 
+      border-slate-400 p-1 px-2 hover:scale-[102%] ease-in-out 
+      transition-all hover:shadow-xl ${color ? `bg-${color}` : ""}`}
+    >
+      {icon}
+      {text}
+    </a>
   );
 };
 
-const ReversedProjectCard = ({
-  title,
-  description,
-  tools,
-  image,
-  color,
-  githubLink,
-  liveLink,
-}) => {
+const ToolsList = ({ tools }) => (
+  <ul className="flex flex-row gap-2 py-6">
+    {tools.map((tool, index) => (
+      <li key={index}>
+        <img src={`/${tool}.png`} alt={tool} width="40px" height="40px" title={tool} />
+      </li>
+    ))}
+  </ul>
+);
+
+const ProjectCardContent = ({ title, description, tools, color, githubLink, liveLink, reversed }) => {
   return (
-    <>
-      <div
-        className={`border mx-auto shadow-lg h-1/4 
-        grid grid-cols-projectsCardReversed
-        grid-rows-5 max-w-[1000px] bg-white`}
-      >
-        <div
-          className="h-full row-span-5 overflow-hidden object-center
-        flex items-center justify-center "
-        >
-          <img src={image} className=" max-h-[340px]" />
-        </div>
-        <div
-          className="row-span-5 flex flex-col items-end justify-around pr-8 py-8
-        "
-        >
-          <h1 className="text-lg pb-2">{title}</h1>
-          <div className="h-[1px] w-1/2 bg-black/10 mb-6"></div>
-          <p className="max-w-[400px] text-right">{description}</p>
-          <div className="flex flex-row gap-4 pt-6">
-            <a
-              href={liveLink}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={` flex items-center justify-center 
-        gap-1 border shadow-lg border-slate-400 p-1 px-2 
-        hover:scale-[102%] ease-in-out transition-all hover:shadow-xl
-        bg-${color}`}
-            >
-              <BsEye />
-              Live Project
-            </a>
-
-            <a
-              href={githubLink}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center justify-center 
-      gap-1 px-2 shadow-lg border border-slate-400 p-1
-      hover:scale-[102%] ease-in-out transition-all hover:shadow-xl"
-            >
-              <BsCode />
-              Source Code
-            </a>
-          </div>
-
-          <ul className="flex flex-row gap-2 py-6">
-            {tools.map((tool, index) => (
-              <li key={index}>
-                <img
-                  src={`/${tool}.png`}
-                  alt={tool}
-                  width="40px"
-                  height="40px"
-                  title={tool}
-                />
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className={`h-full w-2 row-span-5 bg-${color}`}></div>
+    <div className={`row-span-5 flex flex-col ${reversed ? "items-end" : "items-start"} justify-around p-8`}>
+      <h1 className="text-lg pb-2">{title}</h1>
+      <div className="h-[1px] w-1/2 bg-black/10 mb-6"></div>
+      <p className={`max-w-[400px] ${reversed ? "text-right" : ""}`}>{description}</p>
+      <div className="flex flex-row gap-4 pt-6">
+        <LinkButton link={liveLink} color={color} icon={<BsEye />} text="Live Project" />
+        <LinkButton link={githubLink} icon={<BsCode />} text="Source Code" />
       </div>
-    </>
+      <ToolsList tools={tools} />
+    </div>
   );
 };
+
+const ProjectCard = (props) => {
+  return (
+    <div className={`border mx-auto shadow-lg h-1/4 grid grid-cols-projectsCard grid-rows-5 max-w-[1000px] bg-white`}>
+      <div className={`h-full w-2 row-span-5 bg-${props.color}`}></div>
+      <ProjectCardContent {...props} />
+      <div className="h-full row-span-5 overflow-hidden object-center flex items-center justify-center">
+        <img src={props.image} className=" max-h-[340px]" />
+      </div>
+    </div>
+  );
+};
+
+const ReversedProjectCard = (props) => {
+  return (
+    <div className={`border mx-auto shadow-lg h-1/4 grid grid-cols-projectsCardReversed grid-rows-5 max-w-[1000px] bg-white`}>
+      <div className="h-full row-span-5 overflow-hidden object-center flex items-center justify-center">
+        <img src={props.image} className=" max-h-[340px]" />
+      </div>
+      <ProjectCardContent {...props} reversed />
+      <div className={`h-full w-2 row-span-5 bg-${props.color}`}></div>
+    </div>
+  );
+};
+
 
 const Projects = () => {
   return (
